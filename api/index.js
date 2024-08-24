375305acb6cbf7654b1382ac6781e205322c488f
Pushpender <pushpenderrajputsp@gmail.com>
2024-08-24 14:33:19 +0530

    vercel add

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const App = express();
+
+App.get('/', async (req, res) => {
+  res.json({
+    msg: "Its the HOME Page"
+  });
+});
+
+App.get('/user', async (req, res) => {
+  res.json({
+    msg: "This is the User Page"
+  });
+});
+
+App.get('/account', async (req, res) => {
+  res.json({
+    msg: "This is accounts Page"
+  });
+});
+
+module.exports = App;
